42749a6656458230471d73f91132658d3558d7e1
Jendrik Seipp <jendrik.seipp@liu.se>
2023-10-14 19:04:35 +0200

    Add CI tests.

---
+++ tests/run-tests.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+set -euxo pipefail
+
+function test() {
+    prefix=${1}
+
+    pdflatex ${prefix}.tex
+    echo
+
+    # Check for bibtex errors.
+    bibtex -min-crossrefs=99 ${prefix}
+
+    # Check for bibtex warnings.
+    # Crossrefs without author/editor will show up as standalone entries at the beginning of the bibliography,
+    # but they occur too often to be worth fixing.
+    (bibtex -min-crossrefs=99 ${prefix} | grep "Warning--" | grep -v "Warning--to sort, need") && return 1 || return 0
+}
+
+# Change into the directory of this script.
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+cd paper
+export BIBINPUTS=../../
+test "paper"
+test "paper-short"
+echo "All tests passed."
