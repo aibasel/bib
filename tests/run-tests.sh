4a052defa9090519e386754af7328770f40294be
Jendrik Seipp <jendrik.seipp@liu.se>
2023-10-14 18:35:44 +0200

    Fix tests.

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -14,7 +14,7 @@ function test() {
     # Check for bibtex warnings.
     # Crossrefs without author/editor will show up as standalone entries at the beginning of the bibliography,
     # but they occur too often to be worth fixing.
-    (bibtex -min-crossrefs=99 ${prefix} | grep "Warning--" | grep -v "Warning--to sort, need") && exit 1 || exit 0
+    (bibtex -min-crossrefs=99 ${prefix} | grep "Warning--" | grep -v "Warning--to sort, need") && return 1 || return 0
 }
 
 # Change into the directory of this script.
@@ -23,3 +23,4 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 cd paper
 test "paper"
 test "paper-short"
+echo "All tests passed."
